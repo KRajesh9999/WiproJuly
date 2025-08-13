e3fe56f413a72c5584b62b7fcf539669f9657ad5
KRajesh9999 <kethireddyrajesh9999@gmail.com>
2025-08-13 22:09:57 +0530

    Day20 Contents

---
+++ Day20_Rest_React_BankingProject/BankProjectCore/Models/BankDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace BankProjectCore.Models
+{
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext() { }
+
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
+        {
+        }
+
+        //OnConfiguring() method is used to select and configure the data source
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().ToTable("Account");
+            modelBuilder.Entity<Trans>().ToTable("Trans");
+            modelBuilder.Entity<Login>().ToTable("Login");
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Trans> Trans { get; set; }
+
+        public DbSet<Login> Logins { get; set; }
+
+    }
+}
